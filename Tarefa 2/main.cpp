36ffc7f87c13625230d9a2479197e27765c9299f
JuCarv-bit <juliana.souza.opp@gmail.com>
2024-03-16 19:56:11 -0300

    Start shuffle

---
+++ Tarefa 2/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    const int NUM_MAX = argc < 2 ? 1 : atoi(argv[1]);
+
+    vector<int> indices(NUM_MAX);
+    for (int i = 0; i < NUM_MAX; ++i) {
+        indices[i] = i;
+    }
+
+    random_device rd;
+    mt19937 g(rd()); // Mersenne Twister engine for good randomness
+
+    shuffle(indices.begin(), indices.end(), g);
+
+    vector<bool> shuffled_numbers(NUM_MAX);
+    for (int i = 0; i < NUM_MAX; ++i) {
+        shuffled_numbers[i] = false;
+    }
+
+    for (int i = 0; i < NUM_MAX; ++i) {
+        cout << "index (N-1): " << indices[i] << ": " << shuffled_numbers[indices[i]] << endl;
+    }
+
+    return 0;
+}
